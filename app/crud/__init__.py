727d78b23f801f06fd032c41591b0560b874bebf
ssagarrajeshbhai <ssagarrajeshbhai@deloitte.com>
2024-08-09 10:36:25 +0530

    code-commit: registering classes in modules for easy access and avoid import errors.

---
+++ app/crud/__init__.py
@@ -0,0 +1,13 @@
+from .base import CRUDBase
+from .cart import CRUDCart
+from .order import CRUDOrder
+from .pizza import CRUDPizza
+from .user import CRUDUser
+
+__all__ = [
+    'CRUDBase',
+    'CRUDCart',
+    'CRUDOrder',
+    'CRUDPizza',
+    'CRUDUser'
+]
